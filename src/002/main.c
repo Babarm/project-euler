a31952f5055a93539b08f503e835d5994943b395
Kevin Conte <kevin@conte.net>
2019-09-04 09:20:23 -0700

    added code - 002

--- src/002/main.c
+++ src/002/main.c
@@ -4,5 +4,16 @@
 int main(int argc, char ** argv) {
     printf("C:\n\t");
 
+    int result = 0, a = 1, b = 1, fib = 0;
+
+    do {
+        a = b + fib;
+        b = a + fib;
+        fib = a + b;
+        result += fib;
+    } while(fib < 4000000);
+
+    printf("%d\n", result);
+
     return 0;
 }
